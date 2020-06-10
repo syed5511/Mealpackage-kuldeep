2764026e11ab4dd3e13f8a7b441be47ccba05838
keyeypee <keyeypee@gmail.com>
2020-06-11 02:59:33 +0530

    Updates

--- src/common_components/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button'
-import './button.css';
-
-export default function PackageButton() {
-  return (
-    <Button className="package_button" variant="primary">Primary</Button>
-  );
-}
