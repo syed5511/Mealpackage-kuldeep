c5964a605f64fdb29ecbcdc760ca2b955e2273e1
keyeypee <keyeypee@gmail.com>
2020-07-11 01:07:21 +0530

    small change

--- src/pages/__test__/OnMenuPage.test.js
+++ src/pages/__test__/OnMenuPage.test.js
@@ -2,6 +2,5 @@ import OnMenuPage from "../on-menu";
 import getSnapShot from "../../utils/getSnapShot";
 
 test("OnMenu page snapshot", () => {
-  const component = getSnapShot(OnMenuPage)();
-  expect(component.root.findByType("button").props.onClick()).toBe(2);
+  getSnapShot(OnMenuPage)();
 });
